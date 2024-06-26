02947e4fe5fe1b2277707c1f1a110c2076b3c0f7
Yasir Nabil <yasir.nabil5343@gmail.com>
2024-06-26 13:20:27 +0600

    Commit-008: Company API added

---
+++ src/routers/company_router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { body } = require("express-validator");
+const apiEnum = require('../utils/api_constant');
+const companyController = require('../controllers/company_controller');
+const { process_query } = require("../middlewares/process_query");
+const { authenticateToken } = require("../middlewares/token_authenticator");
+
+const router = express.Router();
+
+// ? API to create a company
+router.post(apiEnum.CREATE, companyController.create);
+
+// ? API to get all company using querystring
+router.get(apiEnum.GET_ALL, process_query, companyController.getAllCompany);
+
+// ? API to get company by ID
+router.get(apiEnum.GET_BY_ID, companyController.getCompanyByID);
+
+// ? API to update company by ID
+router.put(apiEnum.UPDATE_BY_ID, authenticateToken, companyController.updateCompanyByID);
+
+// ? API to delete company by ID
+router.delete(apiEnum.DELETE_BY_ID, authenticateToken, companyController.deleteCompanyByID);
+
+module.exports = router;
