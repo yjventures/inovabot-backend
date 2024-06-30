6b9673ffc6c6b2a71b595ce84c8b1e1198c7fc83
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2024-06-30 16:22:38 +0600

    Commit-013 subscription related all api , controller services added

--- src/utils/stripe_utils.js
+++ src/utils/stripe_utils.js
@@ -1,5 +1,37 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 require("dotenv").config();
+const { currency } = require("../utils/enums");
+const { createError } = require("../common/error");
+
+// ^ create product and price utils
+const createProduct = async (
+  name,
+  unit_amount,
+  currency,
+  interval,
+  interval_count = 1
+) => {
+  try {
+    const product = await stripe.products.create({
+      name,
+    });
+
+    const price = await stripe.prices.create({
+      unit_amount, // Price amount in the smallest currency unit (e.g., cents for USD)
+      currency: currency ?? currency.USD,
+      product: product.id, // Associate the price with the product created above
+      nickname: name, // Set the nickname for the price
+      recurring: {
+        interval, // Example: setting a monthly recurring price
+        interval_count,
+      },
+    });
+
+    return price;
+  } catch (error) {
+    throw createError(500, "product price creation failed");
+  }
+};
 
 // ^ create stripe customer
 const createStripeCustomer = async (email) => {
@@ -9,12 +41,12 @@ const createStripeCustomer = async (email) => {
     });
     return customer;
   } catch (err) {
-    throw createError(500, 'Create Stripe Customer error');
+    throw createError(500, "Create Stripe Customer error");
   }
 };
 
 // ^ Create a checkout session for a subscription
-const subscriptionSession = async (priceId, userCustomerId) => {
+const subscriptionSession = async (priceId, stripeCustomerId) => {
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
@@ -25,14 +57,14 @@ const subscriptionSession = async (priceId, userCustomerId) => {
           quantity: 1,
         },
       ],
-      customer: userCustomerId, // customer: "cus_PgjmeMfjbx7SmI",
+      customer: stripeCustomerId, // customer: "cus_PgjmeMfjbx7SmI",
       success_url: process.env.STRIPE_SUCCESS_URL,
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
-    
-    return session.url;
+
+    return session;
   } catch (error) {
-    throw createError(500, 'Checkout Session Error');
+    throw createError(500, "Checkout Session Error");
   }
 };
 
@@ -48,7 +80,7 @@ const getAllSubscription = async (userCustomerId) => {
 
     return subscriptions;
   } catch (error) {
-    throw createError(500, 'Error for show stripe subscriptions');
+    throw createError(500, "Error for show stripe subscriptions");
   }
 };
 
@@ -56,4 +88,5 @@ module.exports = {
   createStripeCustomer,
   subscriptionSession,
   getAllSubscription,
+  createProduct,
 };
