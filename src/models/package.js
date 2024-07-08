8106d6345a97c0942a7fe76673a028e709b6e6f1
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2024-07-08 16:04:23 +0600

    Commit-017: change stripe product creation

--- src/models/package.js
+++ src/models/package.js
@@ -1,4 +1,75 @@
+// const mongoose = require("mongoose");
+
+// const packageSchema = mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       default: '',
+//     },
+//     duration: {
+//       type: String,
+//       default: '',
+//     },
+//     price: {
+//       type: Number,
+//       default: 0,
+//     },
+//     image: {
+//       type: String,
+//       default: '',
+//     },
+//     offer_price: {
+//       type: String,
+//       default: '',
+//     },
+//     hasOffer: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     stripe_price_id: {
+//       type: String,
+//       default: '',
+//     }
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Package = mongoose.model("Package", packageSchema);
+// module.exports = Package;
+
 const mongoose = require("mongoose");
+
+const featureSchema = mongoose.Schema({
+  feature: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String, // Store the type as a string, e.g., 'Number', 'Boolean'
+    required: true,
+  },
+  value: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+});
+
+const priceSchema = mongoose.Schema({
+  price: {
+    type: String,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  stripe_id: {
+    type: String,
+    required: true,
+  },
+});
 
 const packageSchema = mongoose.Schema(
   {
@@ -6,30 +77,15 @@ const packageSchema = mongoose.Schema(
       type: String,
       default: '',
     },
-    duration: {
+    description: {
       type: String,
       default: '',
     },
+    features: [featureSchema],
     price: {
-      type: Number,
-      default: 0,
+      monthly: priceSchema, 
+      yearly: priceSchema,
     },
-    image: {
-      type: String,
-      default: '',
-    },
-    offer_price: {
-      type: String,
-      default: '',
-    },
-    hasOffer: {
-      type: Boolean,
-      default: false,
-    },
-    stripe_price_id: {
-      type: String,
-      default: '',
-    }
   },
   {
     timestamps: true,
