c19e420cb19ca874ea3676d8d94842062fb0f72a
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2024-07-15 10:50:24 +0600

    Commit-19: update middleware

---
+++ src/middlewares/package_feature.js
@@ -0,0 +1,74 @@
+const Package = require("../models/package");
+const Subscription = require("../models/subscription");
+const User = require("../models/user");
+const Company = require("../models/company");
+
+const formatValue = (val)=>{
+  if (val === 'true' || val === "Yes") {
+    return true;
+  } else if (val === 'false' || val === "No") {
+    return false;
+  } else if (val === 'undefined') {
+    return undefined;
+  } else if (val === 'null') {
+    return null;
+  } 
+  return val;
+}
+const packageFeature = async (req, res, next) => {
+  try {
+    const currentUser = req?.user;
+    if (!currentUser?.id) {
+      return res.status(401).json({ message: "User is required" });
+    }
+    const user = await User.findById(currentUser.id).lean();
+    if (!user) {
+      return res.status(404).json({ message: "User not exist" });
+    }
+
+    const company = await Company.findById(user?.company_id).lean();
+
+    if (!company) {
+      return res.status(404).json({ message: "User company not found" });
+    }
+
+    const subscription = await Subscription.findOne({
+      company_id: company._id,
+    }).lean();
+
+    if (!subscription) {
+      return res.status(404).json({ message: "User subscription not found" });
+    }
+
+    const package = await Package.findById(subscription.package_id).lean();
+
+    if (!package) {
+      return res.status(404).json({ message: "Package not found" });
+    }
+
+    const features = package.features;
+    const value = {};
+
+    for (let feature of features) {
+      value[feature.keyword] = formatValue(feature.value);
+    }
+
+    req.body.package = {
+      ...package,
+      ...value,
+    };
+    console.log(req.body.package);
+
+    next();
+  } catch (error) {
+    console.error("Error in findFeatureMiddleware:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { packageFeature };
+
+/*
+  use this like any where for find any feature value
+  const feature1Value = package.value['feature1'];
+*/
--- src/routers/company_router.js
+++ src/routers/company_router.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const { body } = require("express-validator");
-const apiEnum = require('../utils/api_constant');
-const companyController = require('../controllers/company_controller');
+const apiEnum = require("../utils/api_constant");
+const companyController = require("../controllers/company_controller");
 const { process_query } = require("../middlewares/process_query");
 const { authenticateToken } = require("../middlewares/token_authenticator");
+const { packageFeature } = require("../middlewares/package_feature");
 
 const router = express.Router();
 
@@ -14,12 +15,25 @@ router.post(apiEnum.CREATE, authenticateToken, companyController.create);
 router.get(apiEnum.GET_ALL, process_query, companyController.getAllCompany);
 
 // ? API to get company by ID
-router.get(apiEnum.GET_BY_ID, companyController.getCompanyByID);
+router.get(
+  apiEnum.GET_BY_ID,
+  authenticateToken,
+  packageFeature,
+  companyController.getCompanyByID
+);
 
 // ? API to update company by ID
-router.put(apiEnum.UPDATE_BY_ID, authenticateToken, companyController.updateCompanyByID);
+router.put(
+  apiEnum.UPDATE_BY_ID,
+  authenticateToken,
+  companyController.updateCompanyByID
+);
 
 // ? API to delete company by ID
-router.delete(apiEnum.DELETE_BY_ID, authenticateToken, companyController.deleteCompanyByID);
+router.delete(
+  apiEnum.DELETE_BY_ID,
+  authenticateToken,
+  companyController.deleteCompanyByID
+);
 
 module.exports = router;
