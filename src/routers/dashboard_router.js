5d13f101e98e10ee68532702fc7146300df3dc41
Yasir Nabil <yasir.nabil5343@gmail.com>
2024-09-27 15:18:29 +0600

    Commit-090: Refactor user service API and dashboard api

--- src/routers/dashboard_router.js
+++ src/routers/dashboard_router.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const apiEnum = require('../utils/api_constant');
-const passwordController = require('../controllers/password_controller');
 const { process_query } = require("../middlewares/process_query");
 const dashboardController  = require('../controllers/dashboard_controller');
+const { authenticateToken } = require("../middlewares/token_authenticator");
 
 const router = express.Router();
 
 // ? API to request for send invitation
-router.get(apiEnum.ANALYTICS_TOTAL_DATA, dashboardController.totalInformationAnalyticsController);
+router.get(apiEnum.ANALYTICS_TOTAL_DATA, authenticateToken, dashboardController.totalInformationAnalyticsController);
 
 router.get(apiEnum.TOTAL_INCOME, dashboardController.totalIncomeController);
 
-router.get(apiEnum.DASHBOARD_SEARCH, dashboardController.dashboardSearchController);
+router.get(apiEnum.DASHBOARD_SEARCH, authenticateToken, dashboardController.dashboardSearchController);
 
 // ? API to request for check temp password
 // router.post(apiEnum.CHECK_PASSWORD, invitationController.checkTempPasswordController);
